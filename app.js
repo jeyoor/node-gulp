969aa9a585e9aefadf10083daab81cb4b07f62bc
Jeyan Oorjitham <jeyoor@gmx.com>
2013-05-25 16:16:31 -0500

    switch to express shortcut

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ var express = require('express');
 var app = express();
 app.get('/info.json', function(req, res){
   var body = 'Hello, world';
-  res.setHeader('Content-type', 'text/plain');
-  res.setHeader('Content-length', body.length);
-  res.end(body);
+  res.send(body);
 });
 app.listen(3000);
 console.log('Listening on port 3000');
